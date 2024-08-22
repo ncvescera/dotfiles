f69aa3a22792de46112d84bd4822300dcfc75607
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-08-22 17:14:29 +0200

    update poshthemes

--- i3/.config/fish/config.fish
+++ i3/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-oh-my-posh init fish --config ~/.poshthemes/slim.omp.json | source
+oh-my-posh init fish --config ~/.config/poshthemes/slim.omp.json | source
 thefuck --alias | source
 neofetch
 
